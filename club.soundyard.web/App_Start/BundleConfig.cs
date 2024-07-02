0ba7b849eebad9daa1ce12295d97696d26223605
krchdavi <krchdavi@fit.cvut.cz>
2024-07-02 18:44:47 +0200

    Changed db to mssql express

--- club.soundyard.web/App_Start/BundleConfig.cs
+++ club.soundyard.web/App_Start/BundleConfig.cs
@@ -23,10 +23,8 @@ namespace club.soundyard.web
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-             
-                        "~/Content/bootstrap.css",
-                      
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",           
+                        "~/Content/site.css"));
         }
     }
 }
