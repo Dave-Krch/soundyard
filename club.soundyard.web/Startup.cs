e83926a86c230d481f064b46ac4c5dff6e73f7ac
krchdavi <krchdavi@fit.cvut.cz>
2024-07-02 16:50:20 +0200

    Add project files.

---
+++ club.soundyard.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(club.soundyard.web.Startup))]
+namespace club.soundyard.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
